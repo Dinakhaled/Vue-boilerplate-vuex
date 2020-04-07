2b1185a7fb168733f2c0eb5d2e2700fc276d5d09
Dinakhaled <dina.elghndour@gmail.com>
2020-04-07 09:02:14 +0200

    install bootstrap and put the main files

---
+++ src/components/Button.vue
@@ -0,0 +1,16 @@
+<template>
+  <button type="button" class="btn btn-primary">{{text}}</button>
+</template>
+
+<script>
+  export default {
+    name: 'Button',
+    props: {
+      text: String
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container-fluid">
+    <Button :text="'Sample button'" />
+  </div>
+</template>
+
+<script>
+import Button from "../components/Button";
+
+  export default {
+    name: 'Home',
+    components: { Button }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
